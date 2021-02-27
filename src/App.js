559078c5a099f362c6224f871bb1aed6bb049841
Ash Anand <cppanand@gmail.com>
2021-02-26 22:22:31 -0500

    Add normalize.css and setup style structure

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import Navbar from './components/Navbar/Navbar';
 import LoginForm from './components/LoginForm/LoginForm';
 
+import './styles/styles.scss';
+
 function App() {
   return (
     <div>
